d5df14cc7a1c038647a3fdec64540ce4a923b7fb
fsubal <subal@pixiv.co.jp>
2025-09-07 08:05:40 +0900

    refactor

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -6,55 +6,27 @@ import { ScrollTo } from "./ScrollTo";
 import { Icon } from "./Icon";
 import { useAnimation } from "./AnimatedLogo/useAnimation";
 
+const fadeInLater = clsx(
+  "animate-[1s_linear_1.2s_1_normal_running_forwards_fadein]",
+  "opacity-0",
+);
+
 export function Jumbotron() {
-  const { el, visible, playingEnd } = useAnimation(110);
+  const { el, visible, playingEnd } = useAnimation(/** frames */ 110);
 
   return (
     <section
       data-role="jumbotron"
       className={clsx("relative", "overflow-clip")}
     >
-      <div
-        className={clsx(
-          "w-full",
-          "h-screen",
-          "transition-transform",
-          "duration-800",
-          playingEnd ? "scale-105" : "scale-100",
-          "bg-[url('/jumbotron_bg.jpg')]",
-          ["bg-size-[600px_auto]", "screen2:bg-size-[auto_1200px]"],
-          "bg-center",
-          "bg-no-repeat",
-        )}
-      />
-      <div
-        className={clsx(
-          "absolute",
-          "inset-0",
-          "transition-colors",
-          "duration-400",
-          playingEnd ? "bg-white/25" : "bg-primary",
-          "w-full",
-          "h-full",
-          "flex",
-          "flex-col",
-          "items-center",
-          "justify-center",
-          "px-12",
-        )}
-      >
+      <BackgroundImage zoomed={playingEnd} />
+      <BackgroundColor className={playingEnd ? "bg-white/25" : "bg-primary"}>
         <hgroup className="overflow-hidden">
           <h1 className="sr-only">ポッドキャスト百合『ユードットエフエム』</h1>
           <AnimatedLogo ref={el} visible={visible} ended={playingEnd} />
         </hgroup>
 
-        <div
-          className={clsx(
-            "mt-16",
-            "animate-[1s_linear_1.2s_1_normal_running_forwards_fadein]",
-            "opacity-0",
-          )}
-        >
+        <div className={clsx("mt-16", fadeInLater)}>
           <ScrollTo className={clsx("block", "animate-bounce")} href="#main">
             <Icon
               name="24/ArrowDown"
@@ -64,7 +36,47 @@ export function Jumbotron() {
             />
           </ScrollTo>
         </div>
-      </div>
+      </BackgroundColor>
     </section>
   );
 }
+
+const BackgroundImage = ({ zoomed = false }) => (
+  <div
+    className={clsx(
+      "w-full",
+      "h-screen",
+      "transition-transform",
+      "duration-800",
+      zoomed ? "scale-105" : "scale-100",
+      "bg-[url('/jumbotron_bg.jpg')]",
+      ["bg-size-[600px_auto]", "screen2:bg-size-[auto_1200px]"],
+      "bg-center",
+      "bg-no-repeat",
+    )}
+  />
+);
+
+const BackgroundColor = ({
+  className,
+  children,
+}: React.PropsWithChildren<{ className?: string }>) => (
+  <div
+    className={clsx(
+      "absolute",
+      "inset-0",
+      "transition-colors",
+      "duration-400",
+      "w-full",
+      "h-full",
+      "flex",
+      "flex-col",
+      "items-center",
+      "justify-center",
+      "px-12",
+      className,
+    )}
+  >
+    {children}
+  </div>
+);
