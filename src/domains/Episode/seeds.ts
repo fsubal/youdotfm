aca2d5857177cfc73a04217eaffa97197e208504
fsubal <subal@pixiv.co.jp>
2025-09-15 09:20:49 +0900

    implement groupBy

--- src/domains/Episode/seeds.ts
+++ src/domains/Episode/seeds.ts
@@ -3,22 +3,11 @@ import "server-only";
 import { Episode } from "./model";
 import { seed } from "../../utils/seed";
 import yaml from "./seeds.yml";
+import { groupBy } from "../../utils/iterable";
 
 export const episodes: Episode[] = seed(Episode, yaml);
 
-const groupedEpisodes = episodes.reduce(
-  (groups, episode) => {
-    const { numbering } = episode;
-    if (groups[numbering]) {
-      groups[numbering].push(episode);
-    } else {
-      groups[numbering] = [episode];
-    }
-
-    return groups;
-  },
-  {} as { [key: string]: Episode[] },
-);
+const groupedEpisodes = groupBy(episodes, ({ numbering }) => numbering);
 
 export function findEpisodesByNumberings(...numberings: string[]): Episode[] {
   return numberings.flatMap((numbering) => groupedEpisodes[numbering] ?? []);
