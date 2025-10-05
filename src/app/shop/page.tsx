2244a80ee72b21d12ae6bba2eae60e77e2bc46d0
fsubal <subal@pixiv.co.jp>
2025-10-05 20:25:25 +0900

    /shop に掲示

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,14 +3,38 @@ import { Layout } from "../../components/Layout";
 import { products } from "../../domains/Product/seeds";
 import { VarDump } from "../../components/VarDump";
 import { SectionTitle } from "../../components/SectionTitle";
+import { shops } from "../../domains/Shop/seeds";
 
 export default function BuyPage() {
   return (
     <Layout>
       <SectionTitle subheading="Shop">本 &amp; グッズ</SectionTitle>
-      <p>
-        『ユードットエフエム』に関連する商品を紹介します。各種通販サイトで購入できます。
-      </p>
+
+      <div>
+        <p>
+          『ユードットエフエム』に関連する商品を紹介します。各種通販サイトで購入できます。
+        </p>
+        <hr />
+
+        <div className={clsx("my-16", "gap-x-40", "inline-flex", "flex-wrap")}>
+          {shops.map((shop) => (
+            <a
+              className={clsx(
+                "inline-block",
+                "text-sm/loose",
+                ["text-text-500", "hover:text-text-950"],
+                "underline",
+              )}
+              key={shop.kind}
+              href={shop.url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {shop.name}
+            </a>
+          ))}
+        </div>
+      </div>
 
       <div className="not-prose">
         {products.map((item) => (
