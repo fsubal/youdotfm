13444357ab83ab12446b93c1ed096c2d490d9e7b
fsubal <subal@pixiv.co.jp>
2025-10-12 17:10:28 +0900

    番組名を追加

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -2,12 +2,16 @@ import { Layout } from "../../components/Layout";
 import { SectionTitle } from "../../components/SectionTitle";
 import { CharacterListItem } from "../../components/Character/CharacterListItem";
 import { characters } from "../../domains/Character/seeds";
+import clsx from "clsx";
 
 export default function CharactersPage() {
   return (
     <Layout>
       <SectionTitle subheading="Character">キャラクター紹介</SectionTitle>
 
+      <h2 className={clsx("font-bold", "text-lg")}>
+        「📻️ペンときどき音楽」パーソナリティ
+      </h2>
       <div className="divide-y">
         {characters.map((character) => (
           <CharacterListItem key={character.slug} character={character} />
